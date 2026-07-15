3e1de39df01d0ff22711ffa3b7e0dc3ae81e0d21
Dila Efendioğlu <github_dilaefendioglu@gmail.com>
2026-07-15 21:03:49 +0300

    feat: added local and network images, implemented nested Row and Column structures

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,36 @@ class Uygulamam extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 220, 219, 215),
         appBar: AppBar(
           title: Text("uygulamam"),
           backgroundColor: Colors.pinkAccent.shade100,
           centerTitle: true,
         ),
-        body: Text("merhaba"),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("merhaba"),
+                  SizedBox(width: 30),
+                  Image.asset(
+                    'assets/images/search.png',
+                    width: 60,
+                    height: 60,
+                  ),
+                ],
+              ),
+              SizedBox(height: 100),
+              Image.network(
+                'https://media.istockphoto.com/id/1340209535/tr/foto%C4%9Fraf/g%C3%B6kp%C4%B1nar-lake-g%C3%BCr%C3%BCn-sivas.jpg?s=170667a&w=0&k=20&c=4_xKRhqW8Etjc4_NhpO_6-r1AUkSkciAzSNNQDcua7U=',
+                width: 100,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
