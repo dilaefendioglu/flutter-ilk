1806b74c29d832119dc9795549a4027f7c22aeec
Dila Efendioğlu <dilaefendioglu@gmail.com>
2026-07-16 15:42:55 +0300

    feat: create component structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/custom_card.dart';
 
 void main() {
   runApp(Uygulamam());
@@ -22,48 +23,7 @@ class Uygulamam extends StatelessWidget {
         body: Center(
           child: Column(
             children: [
-              Container(
-                margin: EdgeInsets.only(top: 90), //sadece üstten boşluk
-                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 25),
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 247, 244, 244),
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(
-                    color: const Color.fromARGB(255, 237, 223, 228),
-                    width: 3,
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: const Color.fromARGB(255, 230, 138, 196),
-                      blurRadius: 6,
-                      offset: Offset(0, 3),
-                    ),
-                  ],
-                ),
-
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("LOGIN"),
-                    SizedBox(height: 15),
-                    Icon(Icons.person, size: 50),
-                    SizedBox(height: 100),
-
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.lightBlueAccent,
-                      ),
-                      child: Text(
-                        "Giriş yap",
-                        style: TextStyle(
-                          color: const Color.fromARGB(255, 249, 251, 250),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              CustomCardView(),
               SizedBox(height: 50),
               SizedBox(
                 height: 80,
